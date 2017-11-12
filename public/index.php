038ca4b606ad7540fb0b2c1601c6adf66afeea2a
iceman5508 <isaacparker94@gmail.com>
2017-11-12 16:29:22 -0500

    added security

--- public/index.php
+++ public/index.php
@@ -5,6 +5,15 @@ require_once '../bootstrap.php';
 //include welcome component
 require_once 'app/welcome/welcomeComponent.php';
 
+//load in classes being used
+use ITemplate\iExtends\iRouter;
+use IEngine\ibase\iWeb;
+
+
+//protect against illegal navigation
+if(iRouter::routeLimit(2)){
+    iredirect_to(iWeb::projectUrl());
+}
 
 //load in the template
 $welcome = new welcomeComponent('welcome/index.html');
