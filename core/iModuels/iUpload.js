1e2bfa44078be24705f2edeacc91d4fddc39c4ad
iceman5508 <isaacparker94@gmail.com>
2018-03-28 21:51:01 -0400

    upload.js added

---
+++ core/iModuels/iUpload.js
@@ -0,0 +1,42 @@
+/**
+ * Created by Isaac Parker on 3/28/2018.
+ * iUpload file to ajax file upload without jquery
+ */
+function iUpload(id) {
+
+    this.file = document.getElementById(id).files[0];
+
+    /**
+     * Post a file for upload
+     * @param url - The url the data is going to pass to
+     * @param sanitize - The function that cleans the data and and does security checks
+     * before submitting to the server.
+     * @param callback -The call back function, after the file submits.
+     */
+    this.submitFile = function(url,sanitize, callback){
+        var data = new FormData();
+        data.append('iUploadFile', this.file);
+
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function(){
+            if(request.readyState == 4){
+                try {
+                    callback(request.responseText);
+                } catch (e){
+                    var resp = {
+                        status: 'error',
+                        data: 'Unknown error occurred: [' + request.responseText + ']'
+                    };
+                }
+                //console.log(resp.status + ': ' + resp.data);
+            }
+        };
+
+        if(sanitize(this.file)==true) {
+            request.open('POST', url, true);
+            request.send(data);
+        }
+
+    }
+
+}
